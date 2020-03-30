32b446c1114819b4d0c2255c236bc69eb7d38861
Narayanaa <srnarayanaa@gmail.com>
2020-03-30 14:14:47 +0530

    Update interface.py
    Interface to get C code to generate ICG

--- interface.py
+++ interface.py
@@ -1,22 +1,21 @@
 import tkinter as tk
 import subprocess
+import os
 root= tk.Tk()
-
-canvas1 = tk.Canvas(root, width = 600, height = 300)
+root.title('ICG Generator-PCD Package')
+canvas1 = tk.Canvas(root, width = 400, height = 300)
 canvas1.pack()
 
 entry1 = tk.Entry (root) 
-canvas1.create_window(200, 140, width=454, height=200, window=entry1)
+canvas1.create_window(200, 140, width=300, height=200, window=entry1)
 
 def getSquareRoot ():
     x1 = entry1.get()
     print(x1)
-    subprocess.run(["ls"])
     label1 = tk.Label(root, text= float(x1)**0.5)
-    canvas1.create_window(600, 230,width=454, height=20, window=label1)
+    canvas1.create_window(600, 230, window=label1)
     
-button1 = tk.Button(text='Get the Square Root', command=getSquareRoot).pack()
+button1 = tk.Button(text='Get the ICG', command=getSquareRoot).pack()
 canvas1.create_window(200, 500, width=454, height=20, window=button1)
 
-    
 root.mainloop()
