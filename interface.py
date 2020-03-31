e16b52cdfd6a63aded00aa835d917563b433f7c1
Narayanaa <srnarayanaa@gmail.com>
2020-03-31 22:38:49 +0530

    Update interface.py

--- interface.py
+++ interface.py
@@ -9,13 +9,13 @@ canvas1.pack()
 entry1 = tk.Entry (root) 
 canvas1.create_window(200, 140, width=300, height=200, window=entry1)
 
-def getSquareRoot ():
+def run ():
     x1 = entry1.get()
     print(x1)
     label1 = tk.Label(root, text= float(x1)**0.5)
     canvas1.create_window(600, 230, window=label1)
     
-button1 = tk.Button(text='Get the ICG', command=getSquareRoot).pack()
+button1 = tk.Button(text='Get the ICG', command=run).pack()
 canvas1.create_window(200, 500, width=454, height=20, window=button1)
 
 root.mainloop()
